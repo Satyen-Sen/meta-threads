f24c8c081ab0a9a8a51a64554336acf43c779298
Satyen-Sen <satyendra.sant7@gmail.com>
2023-08-08 21:43:35 +0530

    api integrated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,27 @@
-// import { currentUser } from "@clerk/nextjs";
-// import { redirect } from "next/navigation";
-// import ThreadCard from "@/components/cards/ThreadCard";
-// import Pagination from "@/components/shared/Pagination";
-// import { fetchPosts } from "@/lib/actions/thread.actions";
-// import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import ThreadCard from "@/components/cards/ThreadCard";
+import Pagination from "@/components/shared/Pagination";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Home({ searchParams }: { searchParams: { [ key: string ]: string | undefined } }) {
-	// const user = await currentUser();
-	// if (!user) return null;
+	const user = await currentUser();
+	if (!user) return null;
 
-	// const userInfo = await fetchUser(user.id);
-	// if (!userInfo?.onboarded) redirect("/onboarding");
+	const userInfo = await fetchUser(user.id);
+	if (!userInfo?.onboarded) redirect("/onboarding");
 
-	// const result = await fetchPosts(
-	// 	searchParams.page ? +searchParams.page : 1,
-	// 	30
-	// );
+	const result = await fetchPosts(
+		searchParams.page ? +searchParams.page : 1,
+		30
+	);
 
 	return (
 		<>
 			<h1 className='head-text text-left'>Home</h1>
 
-			{/* <section className='mt-9 flex flex-col gap-10'>
+			<section className='mt-9 flex flex-col gap-10'>
 				{result.posts.length === 0 ? (
 				<	p className='no-result'>No threads found</p>
 				) : (
@@ -41,13 +41,13 @@ async function Home({ searchParams }: { searchParams: { [ key: string ]: string 
 						))}
 					</>
 				)}
-			</section> */}
+			</section>
 			
-			{/* <Pagination 
+			<Pagination 
 				path='/'
 				pageNumber={searchParams?.page ? +searchParams.page : 1}
 				isNext={result.isNext}
-			/> */}
+			/>
 		</>
 	);
 }
